3d9769497d652b1c8f89e2e5240a2e5649751ce1
Prince0554 <155054118+Prince0554@users.noreply.github.com>
2025-09-28 22:08:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ try {
 }
 
 // ✅ Group Info
-const GROUP_THREAD_ID = "24196335160017473";
-const LOCKED_GROUP_NAME = "🤪 EXIT FUNNY KIDX + TUSHAR BOKA CHUDKE DAFAN 😂";
+const GROUP_THREAD_ID = "1501364184239302";
+const LOCKED_GROUP_NAME = "🤪 JAY & AARAW CHAUDHARYYYY R4NDDIDIIII K PILLEEE CHUDKE DAFAN 😂";
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
